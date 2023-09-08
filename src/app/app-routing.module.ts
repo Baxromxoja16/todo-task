24e52cb43727a2b8af0efc274dbf39d20b3be201
baxromxoja16 <aglamof@mail.ru>
2023-09-08 16:26:43 +0500

    fix: page titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginGuard } from './login/guards/login.guard';
-import { LoginComponent } from './login/login.component';
-import { TodoFormComponent } from './todo/components/todo-form/todo-form.component';
-import { TodoListComponent } from './todo/components/todo-list/todo-list.component';
 
 const routes: Routes = [
   {
     path: '', redirectTo: 'todo', pathMatch: 'full',
   },
-  { path: 'login', loadChildren: ()=> import('./login/login.module').then(m => m.LoginModule)},
-  { path: 'todo', canActivate: [LoginGuard],  loadChildren: ()=> import('./todo/todo.module').then(m => m.TodoModule)},
+  { path: 'login', loadChildren: ()=> import('./login/login.module').then(m => m.LoginModule), title: 'Login' },
+  { path: 'todo', canActivate: [LoginGuard],  loadChildren: ()=> import('./todo/todo.module').then(m => m.TodoModule), title: 'Todo' },
   {
     path: '**', redirectTo: 'todo', pathMatch: 'full',
   },
